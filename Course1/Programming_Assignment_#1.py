54f23b7584e45048d77b86448e5ac6a278dad4f8
digitalmert <smertatli@gmail.com>
2017-10-22 21:45:17 +0300

    First programming assignment
    Karatsuba's recursive integer multiplication algorithms

---
+++ Course1/Programming_Assignment_#1.py
@@ -0,0 +1,24 @@
+i = 5698452856984528
+j = 5698452856984528
+
+a = j
+b = i
+
+def karatsuba(m, n, leng):
+    if leng == 1:
+        return m * n
+    else:
+        lower1 = m % (10**(leng/2))
+        lower2 = n % (10**(leng/2))
+        upper1 = m / (10**(leng/2))
+        upper2 = n / (10**(leng/2))
+        lower_prod  = karatsuba(lower1, lower2, leng / 2)
+        upper_prod  = karatsuba(upper1, upper2, leng / 2)
+        middle_prod =karatsuba(upper1+lower1, upper2+lower2,(max(len(str(upper1+lower1)), len(str(upper2+lower2))))) - upper_prod - lower_prod
+        return upper_prod * 10**(leng/2*2) + middle_prod * 10**(leng/2) + lower_prod
+t = karatsuba(a,b,len(str(a)))
+print(t)
+print(a*b)
+print(a*b==t)
+print(t-a*b)
+
