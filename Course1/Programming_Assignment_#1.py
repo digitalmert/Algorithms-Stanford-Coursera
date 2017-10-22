1e526fc55b605006c30ad6f1fc2dee8179f42bfa
digitalmert <smertatli@gmail.com>
2017-10-22 21:47:12 +0300

    cleaning

--- Course1/Programming_Assignment_#1.py
+++ Course1/Programming_Assignment_#1.py
@@ -1,8 +1,6 @@
 i = 5698452856984528
 j = 5698452856984528
 
-a = j
-b = i
 
 def karatsuba(m, n, leng):
     if leng == 1:
@@ -16,9 +14,9 @@ def karatsuba(m, n, leng):
         upper_prod  = karatsuba(upper1, upper2, leng / 2)
         middle_prod =karatsuba(upper1+lower1, upper2+lower2,(max(len(str(upper1+lower1)), len(str(upper2+lower2))))) - upper_prod - lower_prod
         return upper_prod * 10**(leng/2*2) + middle_prod * 10**(leng/2) + lower_prod
+
 t = karatsuba(a,b,len(str(a)))
+
 print(t)
 print(a*b)
 print(a*b==t)
-print(t-a*b)
-
